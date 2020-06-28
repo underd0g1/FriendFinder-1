08b5bdc48bbd9ec7c76aba3425092c567f6f08be
underd0g1 <hide4@comcast.net>
2020-06-28 18:20:10 -0400

    added object constructor

--- server.js
+++ server.js
@@ -25,21 +25,25 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//sets bodyparser to handle incoming form data
 app.use(bodyparser.urlencoded({
-   extended: true 
-  }))
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+   extended: true
+ }));
+
+//Define the routes
+
+//index route: this is the route to handle the main landing page. this defines what will happen when the user first requests your url.
 app.get('/', function(request, response){
+  // console. confirmation that the request was made to view the index.html page.
   console.log('user wants to see the index page ... so a request was made');
+  //sending them the home.html page in response to the user requesting our page.
   response.sendFile(__dirname + '/public/home.html');
 });
 
 app.get('/survey', function(request, response){
+  //console confirmation to see the user requested our survey page.
   console.log('user wants to see the survey page ... so a request was made on /survey.');
+  //sending the  actual survey via survey.html
   response.sendFile(__dirname + '/public/testsurvey.html');
 });
 
@@ -49,7 +53,27 @@ app.post('/surveyresponse', function(req,res){
   console.log("this is what you entered for your name: " + req.body.yourName);
   var name = req.body.yourName;
   var title = req.body.title;
-  
+
+  //constuct the objects
+  function Construction(name, q1,q2,q3,q4,q5,q6,q7,q8,q9,q10){
+    this.name = name;
+    this.questions = function(){
+      
+    }
+    // this.q1 = q1;
+    // this.q2 = q2;
+    // this.q3 = q3;
+    // this.q4 = q4;
+    // this.q5 = q5;
+    // this.q6 = q6;
+    // this.q7 = q7;
+    // this.q8 = q8;
+    // this.q9 = q9;
+    // this.q10 = q10;
+
+  }
+
+
 
 
 
